0ddf9e07e3c435000871c9f5776f54ba1dd0d4ce
Oribuin <38044508+Oribuin@users.noreply.github.com>
2019-07-08 17:58:09 +0100

    v1.0

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ bot.on('ready', () => {
     console.log(`==========================`)
     console.log(`Bot Loaded: ${bot.user.tag}`)
     console.log(`Bot Author: Ori#0004`)
-    console.log(`Bot Version: 0.1`)
     console.log(`Servers: ${bot.guilds.size}`)
     console.log(`Users Using Bot: ${bot.users.size}`)
     console.log(`Library: Discord.js`)
@@ -42,6 +41,41 @@ const presence_list = [
         const index = Math.floor(Math.random() * (presence_list.length - 1) + 1);
         bot.user.setActivity(presence_list[index]);
     }, 20000);
+});
+bot.on('guildMemberAdd', member => {
+    let embed = new Discord.RichEmbed();
+    let channel = member.guild.channels.find(ch => ch.name === '👋new-players');
+    embed.setAuthor("Goli Network's Official Discord!")
+    embed.setColor(0xF08080)
+    embed.setDescription(`Welcome to the Goli Network Discord! Be sure to read all of our discord server rules in <#546873908687667200>!\n \nCheck our <#546873898059300867> channel for all of the new updates! Enjoy & Have fun!\nDon't forget to link your account!\n \nEnjoy our server <@${member.id}>`)
+    embed.setImage("https://cdn.discordapp.com/attachments/568862020594565130/580826398193614858/GoliLogo.png")
+    embed.setThumbnail(`$member.user.avatarURL`)
+    embed.setFooter("Type /discord link on goli network.online & follow the instructions to link your account!")
+    embed.addBlankField(true)
+    embed.addField("Join our Minecraft server!",
+    "golinetwork.online")
+    embed.addField("Follow our twitter!",
+    "https://twitter.com/GoliNetwork")
+    embed.addField("Goli Network Website",
+    "Coming Soon!")
+
+    if (!channel) return;
+    channel.send(embed);
+    member.addRoles('555414882636267540', '564056444253634560')
+    console.log(`${member.user.tag} just joined!`)
+});
+bot.on('guildMemberRemove', member => {
+
+    let embed = new Discord.RichEmbed();
+    let channel = member.guild.channels.find(ch => ch.name === 'leaves');
+
+    embed.setAuthor("Member Left")
+    embed.setDescription(`${member.user.tag} just left` + ` the server!`)
+    embed.setThumbnail(member.user.avatarURL)
+
+    if (!channel) return;
+    channel.send(embed)
+    console.log(`${member.user.tag} Just left`)
 });
 
 bot.login(config.token)
