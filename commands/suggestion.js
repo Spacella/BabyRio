eed1d657043f754975a3b85121dcb7ecc465a409
Oribuin <38044508+Oribuin@users.noreply.github.com>
2019-07-10 20:25:20 +0100

    Fixed Args
    Finally fixed the issue I had with the suggestion args.
    
    I didn't pay attention to anything about args & just brushed over it and didn't actually read it

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -9,16 +9,22 @@ module.exports = class suggestion {
 
     run(bot, message, args){
         let embed = new Discord.RichEmbed()
+        //suggestion variables
+        var command = args[0];
+        var suggestion = args.slice(1).join(" ");
+        //Embed
          embed.setTitle(`Upvote | Downvote`)
          embed.setColor(0xF08080)
          embed.setDescription('Suggest features to our server with:\n `+su <suggestion>`')
          embed.setThumbnail(`${message.author.avatarURL}`)
          embed.addBlankField(true)
          embed.addField('**Suggestion**',
-         `${message.author.lastMessage}`)
+         `${suggestion}`)
          embed.addField('**Suggestion by:**',
          `<@${message.author.id}>`)
 
+         //after command is sent
+         if (message.guild.id === '505872328538718233')
          bot.channels.get("574366597515444244").send(embed).then(x=> x.react('597942179838558210').then(x.react('597942166517710868')))
          message.delete();
          }
